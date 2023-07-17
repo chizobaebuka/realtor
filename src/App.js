0a7a693124edd5b2506d7c5883ecc7ab61f95734
Decagon <decagon@Decagons-MacBook-Air.local>
2023-07-17 17:01:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import ForgotPassword from './pages/ForgotPassword';
-import Offers from './pages/Offers';
-import Signin from './pages/Signin';
-import SignUp from './pages/SignUp';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
-          <Route path='/' element={<Home/> }/>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/offers' element={<Offers />} />
-          <Route path='/sign-in' element={<Signin />} />
-          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/' element={<Home />}/>
         </Routes>
       </Router>
     </>
